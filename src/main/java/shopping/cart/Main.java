0f00ac74ff5f6f291edbb69c9bd5d256f74617ed
kazokmr <engkokmr@gmail.com>
2022-06-23 09:30:04 +0900

    Section3: ItemをCartに入れるgRPCサービスを作成
    - 同じCartにItemを追加しても Responseには前に入れたItemsが表示されないのは、まだ永続化されていないから？

--- src/main/java/shopping/cart/Main.java
+++ src/main/java/shopping/cart/Main.java
@@ -4,25 +4,36 @@ import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.management.cluster.bootstrap.ClusterBootstrap;
 import akka.management.javadsl.AkkaManagement;
+import com.typesafe.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import shopping.cart.proto.ShoppingCartService;
 
 public class Main {
 
-  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-  public static void main(String[] args) {
-    ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(), "ShoppingCartService");
-    try {
-      init(system);
-    } catch (Exception e) {
-      logger.error("Terminating due to initialization failure.", e);
-      system.terminate();
+    public static void main(String[] args) {
+        // Main Actorの振る舞いでActorSystemを起動する
+        ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(), "ShoppingCartService");
+        try {
+            init(system);
+        } catch (Exception e) {
+            logger.error("Terminating due to initialization failure.", e);
+            system.terminate();
+        }
     }
-  }
 
-  public static void init(ActorSystem<Void> system) {
-    AkkaManagement.get(system).start();
-    ClusterBootstrap.get(system).start();
-  }
+    public static void init(ActorSystem<Void> system) {
+        // Akka Clusterで利用するためのAkka Managementを初期化する
+        AkkaManagement.get(system).start();
+        ClusterBootstrap.get(system).start();
+
+        // Configファイルから必要な情報をとり、gRPCサーバーを起動する
+        Config config = system.settings().config();
+        String grpcInterface = config.getString("shopping-cart-service.grpc.interface");
+        int grpcPort = config.getInt("shopping-cart-service.grpc.port");
+        ShoppingCartService grpcService = new ShoppingCartServiceImpl();
+        ShoppingCartServer.start(grpcInterface, grpcPort, system, grpcService);
+    }
 }
